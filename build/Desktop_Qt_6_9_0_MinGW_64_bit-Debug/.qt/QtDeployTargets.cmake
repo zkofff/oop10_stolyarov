b5fe8e2cd07539ab00b4af5d2a6541a9a6a8aa00
zkofff <tupaarbuz@gmail.com>
2025-05-12 21:19:35 +0300

    first commit

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_penisdushilin_FILE C:/Users/user/Desktop/penisdushilin/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/penisdushilin.exe)
+set(__QT_DEPLOY_TARGET_penisdushilin_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_penisdushilin_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,11 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.9.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/user/Desktop/penisdushilin/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.9.0/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Qt/Tools/mingw1310_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qt/Tools/mingw1310_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
